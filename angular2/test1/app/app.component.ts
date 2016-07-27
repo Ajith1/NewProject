5118a839f45c3720b8de99e3c704f92c60fc2600
Ajith <ajiththomasmec@gmail.com>
2016-07-27 14:03:16 -0500

    Initial Commit

---
+++ angular2/test1/app/app.component.ts
@@ -0,0 +1,9 @@
+
+
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'baseComponent.html',
+  styleUrls:['baseComponent.css']
+})
+export class AppComponent { }
